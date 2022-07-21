b42722b1b6a67ce2dccf7477d1f66fe0cdd6f335
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-07-21 16:35:04 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let adjective1 = prompt("Give me an adjective.")
+let noun1 = prompt("Give me a noun.");
+let noun2 = prompt("Give me another noun.");
+let adjective2 = prompt("Give me another adjective.");
+let shape1 = prompt("Give me a shape.");
+let place1 = prompt("Give me a place.");
+
+let song = "Twinkle, twinkle, " + adjective1;
+song += " " + noun1 + ", ";
+song += "how I wonder what you are. ";
+song += "Up above the " + noun2 + " so ";
+song += adjective2;
+song += ", like a " + shape1 + " in the " + place1;
+song += ". Twinkle, twinkle, " + adjective1 + " ";
+song += noun1 + ", how I wonder what you are.";
+
+alert(song);
